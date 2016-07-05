369b74520dfec0e62eee713c65ff806d89796ceb
walkcode <walk_code@163.com>
2016-07-05 13:41:49 +0800

    号码归属地查询

---
+++ app/QueryPhone.php
@@ -0,0 +1,40 @@
+<?php
+namespace app;
+use libs\ImHttpRequest;
+/**
+ * Created by PhpStorm.
+ * Author kylin
+ * Date: 2016/6/30
+ * Time: 13:54
+ */
+class QueryPhone
+{
+    const TAOBAO_API = 'https://tcc.taobao.com/cc/json/mobile_tel_segment.html';
+
+    public static function query($phone) {
+
+        if (self::verifyPhone('15363469159')) {
+            ImHttpRequest::request(self::TAOBAO_API,['tel' => $phone]);
+        };
+
+    }
+
+    /*
+     * 校验手机号码是否
+     *  @param $phone
+     *  @return boolean
+     * */
+
+    public static function verifyPhone($phone = null) {
+
+        $ret = false;
+        if ($phone) {
+            if (preg_match('/^1[34578]{1}\d{9}/',$phone)) {
+                $ret = true;
+            }
+        }
+        return $ret;
+
+    }
+
+}
